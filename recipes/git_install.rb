7377c09aeba6b1228675b236877000fa14cade52
saketh-linux <sakethjakkula642@gmail.com>
2018-10-05 12:17:06 +0530

    setup

--- recipes/git_install.rb
+++ recipes/git_install.rb
@@ -4,10 +4,10 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-#execute 'apt update' do
-#  command 'apt-get update'
-#  only_if 'apt list --upgradeable'
-#end
+execute 'apt update' do
+  command 'apt-get update'
+  only_if 'apt list --upgradeable'
+end
 
 package 'git'
 package 'python3-pip'
